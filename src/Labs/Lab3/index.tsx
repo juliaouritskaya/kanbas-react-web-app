abc51c9a5ab033094b81247204eeadca2e874bcf
Julia Ouritskaya <juliaouritskaya.gmail.com>
2024-09-17 18:59:10 -0400

    first commit

---
+++ src/Labs/Lab3/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Lab3() {
+    return (
+        <div>
+            <h2>Lab 3</h2>
+            <h3>Exercise 3.1</h3>
+            <h3>Exercise 3.2</h3>
+        </div>
+    )
+}
---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Lab4() {
+    return (
+        <div>
+            <h2>Lab 4</h2>
+            <h3>Exercise 4.1</h3>
+            <h3>Exercise 4.2</h3>
+        </div>
+    )
+}
