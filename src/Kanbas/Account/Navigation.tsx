389e9c0be5a547746a1d593c66e6a26c5f172d23
Julia Ouritskaya <juliaouritskaya.gmail.com>
2024-12-05 15:49:59 -0500

    a6

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,41 +1,21 @@
-import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import {useLocation} from "react-router";
-
 export default function AccountNavigation() {
-    const {currentUser} = useSelector((state: any) => state.accountReducer);
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
     const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
-    const {pathname} = useLocation();
-
+    const active = (path: string) => (pathname.includes(path) ? "active" : "");
+    const { pathname } = useLocation();
     return (
-        <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-            {/* Render Signin and Signup links only if no user is signed in */}
-            {!currentUser && (
-                <>
-                    <Link
-                        to={`/Kanbas/Account/Signin`}
-                        className={`list-group-item ${pathname === "/Kanbas/Account/Signin" ? "active" : ""} border border-0`}
-                    >
-                        Signin
-                    </Link>
-                    <Link
-                        to={`/Kanbas/Account/Signup`}
-                        className={`list-group-item ${pathname === "/Kanbas/Account/Signup" ? "active" : ""} text-danger border border-0`}
-                    >
-                        Signup
-                    </Link>
-                </>
-            )}
-
-            {/* Render Profile link only if a user is signed in */}
-            {currentUser && (
-                <Link
-                    to={`/Kanbas/Account/Profile`}
-                    className={`list-group-item ${pathname === "/Kanbas/Account/Profile" ? "active" : ""} text-danger border border-0`}
-                >
-                    Profile
+        <div id="wd-account-navigation" className="wd list-group rounded-0">
+            {links.map((link) => (
+                <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item border-0 ${active(link)}`}>
+                    {link}
                 </Link>
-            )}
+            ))}
+            {currentUser && currentUser.role === "ADMIN" && (
+                <Link to={`/Kanbas/Account/Users`} className={`list-group-item border-0 ${active("Users")}`}>
+                    Users
+                </Link> )}
         </div>
-    );
-}
+    );}
